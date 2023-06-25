a00a87bdddde724b1eb8872769f200e625463288
Zia Younas <97870737+ziayounasch@users.noreply.github.com>
2023-06-25 13:49:05 +0500

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/4FYbBdq/Screenshot-1.png"
+        <img src="https://i.ibb.co/4FYbBdq/Screenshot-1.png">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
